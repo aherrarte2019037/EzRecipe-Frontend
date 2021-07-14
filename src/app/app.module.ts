359e3d0cb2779cc72f468cda4750cc6d54fdb1f4
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-07-14 14:30:15 -0600

    Se agregaron páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/main/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbDatepickerModule, NbThemeModule } from '@nebular/theme';
+import { NbThemeModule } from '@nebular/theme';
 import { NebularModule } from './nebular.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot(),
+    NbThemeModule.forRoot({ name: 'default' }),
     NebularModule
   ],
   providers: [],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   mode: 'jit',
   purge: [ './src/**/*.{html, ts}', './projects/**/*.{html, ts}' ],
-  darkMode: false,
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('daisyui')
-  ],
+  plugins: [],
 }
