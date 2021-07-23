dbd66fe6f558e1485aaee720ad1496755cb924c8
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-07-22 20:51:48 -0600

    Se creo componente add-recipe

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,24 +1,45 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NebularModule } from '../nebular.module';
 import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 import { LatestRecipesComponent } from './latest-recipes/latest-recipes.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 @NgModule({
   declarations: [
     NavbarComponent,
     RecipeCardComponent,
-    LatestRecipesComponent
+    LatestRecipesComponent,
+    AddRecipeComponent
   ],
   imports: [
     CommonModule,
-    NebularModule
+    NebularModule,
+    ReactiveFormsModule,
+    PerfectScrollbarModule
   ],
   exports: [
     NavbarComponent,
     RecipeCardComponent,
-    LatestRecipesComponent
-  ]
+    LatestRecipesComponent,
+    NebularModule
+  ],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ComponentsModule { }
