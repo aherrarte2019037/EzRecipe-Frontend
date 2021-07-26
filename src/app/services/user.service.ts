d7c4821859a81af753435ccdd49d79f63c5e941f
José López <jlopez-2019044@kinal.edu.gt>
2021-07-26 13:44:15 -0600

    Se crearon las opciones de ascender o cancelar chef

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -27,4 +27,22 @@ export class UserService {
     ).subscribe();
   }
 
+  getChefRequests(): Observable<any>{
+
+    return this.http.get(`${this.apiUrl}/getChefRequests`);
+
+  }
+
+  confirmChefRquest(id: String):Observable<any>{
+
+    return this.http.get(`${this.apiUrl}/confirmChefRequest/${id}`);
+
+  }
+
+  cancelChefRequest(id: String):Observable<any>{
+
+    return this.http.get(`${this.apiUrl}/cancelChefRequest/${id}`);
+
+  }
+
 }
