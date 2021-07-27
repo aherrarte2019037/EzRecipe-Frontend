ac257d230a8e1fd965b40006109af54410ba3c8c
José Góngora <jdavidgongorao@gmail.com>
2021-07-26 19:13:18 -0600

    Diseño perfil de usuario

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent implements OnInit {
+  profileForm: FormGroup = this.buildForm()
+  formActivated: boolean = false;
+  formChanges: any = {};
+  userLogged: any;
+  userProfile: any;
+  username: any
 
-  constructor() { }
+  constructor(private renderer: Renderer2, public userService: UserService, private formBuilder: FormBuilder, private _route: ActivatedRoute) {
+    this._route.queryParams.subscribe(params=>{ this.username = params['username'] || null })
+   }
 
   ngOnInit(): void {
+    this.username = this._route.snapshot.paramMap.get('username')
+    this.userService.userLogged.subscribe( data =>{ this.userLogged = data
+      if(this.username === 'user'){
+        this.userService.getUserUsername(data.username).subscribe( data => this.userProfile = data )
+      }else {
+        this.userService.getUserUsername(this.username).subscribe( data => this.userProfile = data )
+      }
+
+    })
+
+  }
+
+  setFormValue(){
+    this.profileForm.patchValue( this.userLogged );
+    this.profileForm.valueChanges.subscribe( value => {
+      if(
+        value.username === this.userLogged.username
+        && value.email === this.userLogged.email
+        && value.name === this.userLogged.name
+        && value.lastname === this.userLogged.lastname) return this.formActivated = false;
+
+        if( value.name !== this.userLogged.name ) this.formChanges.name = value.name
+        if( value.lastname !== this.userLogged.lastname ) this.formChanges.lastname = value.lastname
+        if( value.email !== this.userLogged.email ) this.formChanges.email = value.email
+        if( value.username !== this.userLogged.username ) this.formChanges.username = value.username
+        return this.formActivated = true;
+    } )
+  }
+
+  buildForm(){
+    const form = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      profileImg: [''],
+    });
+
+    return form;
+
+  }
+
+  showEdit( edit: HTMLElement, close: HTMLElement, action: boolean ){
+    if(action){
+      this.renderer.setStyle(edit, 'display', 'none')
+      this.renderer.setStyle(edit, 'display', 'block')
+
+    }else {
+      this.renderer.setStyle( edit, 'display', 'block' )
+      this.renderer.setStyle( close, 'display', 'none' )
+    }
+
+  }
+
+  editProfile(){
+    this.userService.editUser(this.formChanges, this.userLogged._id).subscribe(
+      () => {
+
+        this.formChanges = {};
+      },
+      error => {
+        console.log(<any>error)
+      }
+    )
+    this.desactiveForm();
+
+  }
+
+  desactiveForm(){
+    this.formActivated = false;
   }
 
 }
